0bcf8d850e2c4c242b8ab4a4b602a37395553432
tonymanou <tonymanou@gmail.com>
2014-12-05 13:21:01 +0100

    Regrouped all "type*attr" controllers in one file

--- src/main/java/fr/epf/batmeca/controller/TypeMatAttrController.java
+++ src/main/java/fr/epf/batmeca/controller/TypeMatAttrController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,43 +15,46 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import fr.epf.batmeca.entity.TypeMaterialAttribute;
 import fr.epf.batmeca.service.ITypeMaterialAttributService;
 
-/**
- * Servlet implementation class AddTypeAttrMat
- */
 @Controller
-@RequestMapping("/AddTypeAttrMat")
-public class AddTypeAttrMatServlet {
+public class TypeMatAttrController {
 
 	@Autowired
-	private ITypeMaterialAttributService typeMatAttrService;
+	private ITypeMaterialAttributService typeService;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	@RequestMapping(method = RequestMethod.GET)
-	protected void doGet(HttpServletRequest request,
+	@RequestMapping(value = "/AddTypeAttrMat", method = RequestMethod.GET)
+	protected void addTypeAttrMatGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher(response
 				.encodeURL("/WEB-INF/addTypeAttrMat.jsp"));
 		rd.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	@RequestMapping(method = RequestMethod.POST)
-	protected void doPost(HttpServletRequest request,
+	@RequestMapping(value = "/AddTypeAttrMat", method = RequestMethod.POST)
+	protected void addTypeAttrMatPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		/*
-		 * Création objet associé
-		 */
 		TypeMaterialAttribute type = new TypeMaterialAttribute();
 		type.setName(request.getParameter("inputName"));
 		type.setPattern(request.getParameter("inputPattern"));
-		// enregistrement en base de données
-		typeMatAttrService.add(type);
+
+		typeService.add(type);
 		response.sendRedirect(response.encodeURL("./Config"));
+	}
+
+	@RequestMapping(value = "/RemoveTypeMatAttr", method = RequestMethod.GET)
+	protected void removeTypeMatAttrGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		TypeMaterialAttribute type;
+		type = typeService.find(Integer.parseInt(request.getParameter("id")));
+		typeService.remove(type);
+
+		RequestDispatcher rd = null;
+		rd = request.getRequestDispatcher("/Config");
+		rd.forward(request, response);
+	}
+
+	@RequestMapping(value = "/RemoveTypeMatAttr", method = RequestMethod.POST)
+	protected void removeTypeMatAttrPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 	}
 }
--- src/main/java/fr/epf/batmeca/controller/TypeTestAttrController.java
+++ src/main/java/fr/epf/batmeca/controller/TypeTestAttrController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,34 +15,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import fr.epf.batmeca.entity.TypeTestAttribute;
 import fr.epf.batmeca.service.ITypeTestAttributService;
 
-/**
- * Servlet implementation class AddTypeTestAttrServlet
- */
 @Controller
-@RequestMapping("/AddTypeTestAttr")
-public class AddTypeTestAttrServlet {
+public class TypeTestAttrController {
 
 	@Autowired
 	private ITypeTestAttributService typeService;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	@RequestMapping(method = RequestMethod.GET)
-	protected void doGet(HttpServletRequest request,
+	@RequestMapping(value = "/AddTypeTestAttr", method = RequestMethod.GET)
+	protected void addTypeTestAttrGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher(response
 				.encodeURL("/WEB-INF/addTypeTestAttr.jsp"));
 		rd.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	@RequestMapping(method = RequestMethod.POST)
-	protected void doPost(HttpServletRequest request,
+	@RequestMapping(value = "/AddTypeTestAttr", method = RequestMethod.POST)
+	protected void addTypeTestAttrPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// créattion objet associé
 		TypeTestAttribute type = new TypeTestAttribute();
@@ -52,5 +39,23 @@ public class AddTypeTestAttrServlet {
 		// enregistrement en base de données
 		typeService.add(type);
 		response.sendRedirect(response.encodeURL("./Config"));
+	}
+
+	@RequestMapping(value = "/RemoveTypeTestAttr", method = RequestMethod.GET)
+	protected void removeTypeTestAttrGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		TypeTestAttribute type;
+		type = typeService.find(Integer.parseInt(request.getParameter("id")));
+		typeService.remove(type);
+
+		RequestDispatcher rd = null;
+		rd = request.getRequestDispatcher("/Config");
+		rd.forward(request, response);
+	}
+
+	@RequestMapping(value = "/RemoveTypeTestAttr", method = RequestMethod.POST)
+	protected void removeTypeTestAttrPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 	}
 }
