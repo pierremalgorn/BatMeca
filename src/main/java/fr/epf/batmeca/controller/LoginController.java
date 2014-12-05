a5eebc9f06762a4af721498de9522bd24f79ed23
tonymanou <tonymanou@gmail.com>
2014-12-05 13:45:18 +0100

    Renamed login controller

--- src/main/java/fr/epf/batmeca/controller/LoginController.java
+++ src/main/java/fr/epf/batmeca/controller/LoginController.java
@@ -5,11 +5,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handle user session
- */
 @Controller
-public class LoginServlet {
+public class LoginController {
 
 	@RequestMapping(value = "/Login", method = RequestMethod.GET)
 	public String login(ModelMap model) {
