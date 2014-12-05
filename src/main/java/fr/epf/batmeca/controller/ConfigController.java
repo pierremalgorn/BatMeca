79875e094600f650be3a3a311fd9ab9c20edfe11
tonymanou <tonymanou@gmail.com>
2014-12-05 13:39:25 +0100

    Regrouped all "config" controllers in one file

--- src/main/java/fr/epf/batmeca/controller/ConfigController.java
+++ src/main/java/fr/epf/batmeca/controller/ConfigController.java
@@ -1,11 +1,11 @@
 package fr.epf.batmeca.controller;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,24 +19,16 @@ import fr.epf.batmeca.entity.TypeTestAttribute;
 import fr.epf.batmeca.service.ITypeMaterialAttributService;
 import fr.epf.batmeca.service.ITypeTestAttributService;
 
-/**
- * Servlet implementation class ConfigServlet
- */
 @Controller
-@RequestMapping("/Config")
-public class ConfigServlet {
+public class ConfigController {
 
 	@Autowired
 	private ITypeMaterialAttributService typeMatAttrService;
 	@Autowired
 	private ITypeTestAttributService typeTestAttrService;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	@RequestMapping(method = RequestMethod.GET)
-	protected void doGet(HttpServletRequest request,
+	@RequestMapping(value = "/Config", method = RequestMethod.GET)
+	protected void configGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		/*
 		 * Recuperation des listes d'attributs
@@ -52,13 +44,34 @@ public class ConfigServlet {
 		rd.forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	@RequestMapping(method = RequestMethod.POST)
-	protected void doPost(HttpServletRequest request,
+	@RequestMapping(value = "/Config", method = RequestMethod.POST)
+	protected void configPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+	}
+
+	@RequestMapping(value = "/ConfigGenerator", method = RequestMethod.GET)
+	protected void configGeneratorGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher(response
+				.encodeURL("/WEB-INF/configGenerator.jsp"));
+		rd.forward(request, response);
+	}
+
+	@RequestMapping(value = "/ConfigGenerator", method = RequestMethod.POST)
+	protected void configGeneratorPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// String post = request.getParameter("POST");
+		// System.out.println(post);
+		Enumeration<String> params = request.getParameterNames();
+
+		while (params.hasMoreElements()) {
+			String string = params.nextElement();
+			// String val = request.getParameter(string);
+
+			System.out.println("Name Field = " + string);
+			// System.out.println("Val Filed = "+val);
+		}
+		configGeneratorGet(request, response);
 	}
 }
