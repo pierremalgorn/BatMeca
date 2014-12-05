9817018d266935533d4a4a0c4efcdce495e08854
tonymanou <tonymanou@gmail.com>
2014-12-05 14:31:49 +0100

    Cleaned "type*attr" controllers

--- src/main/java/fr/epf/batmeca/controller/TypeMatAttrController.java
+++ src/main/java/fr/epf/batmeca/controller/TypeMatAttrController.java
@@ -1,16 +1,10 @@
 package fr.epf.batmeca.controller;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.epf.batmeca.entity.TypeMaterialAttribute;
 import fr.epf.batmeca.service.ITypeMaterialAttributService;
@@ -22,39 +16,31 @@ public class TypeMatAttrController {
 	private ITypeMaterialAttributService typeService;
 
 	@RequestMapping(value = "/AddTypeAttrMat", method = RequestMethod.GET)
-	protected void addTypeAttrMatGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher(response
-				.encodeURL("/WEB-INF/addTypeAttrMat.jsp"));
-		rd.forward(request, response);
+	protected String addTypeAttrMatGet() {
+		return "addTypeAttrMat";
 	}
 
 	@RequestMapping(value = "/AddTypeAttrMat", method = RequestMethod.POST)
-	protected void addTypeAttrMatPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	protected String addTypeAttrMatPost(
+			@RequestParam("inputName") String inputName,
+			@RequestParam("inputPattern") String inputPattern) {
+
 		TypeMaterialAttribute type = new TypeMaterialAttribute();
-		type.setName(request.getParameter("inputName"));
-		type.setPattern(request.getParameter("inputPattern"));
+		type.setName(inputName);
+		type.setPattern(inputPattern);
 
 		typeService.add(type);
-		response.sendRedirect(response.encodeURL("./Config"));
+
+		return "redirect:/Config";
 	}
 
 	@RequestMapping(value = "/RemoveTypeMatAttr", method = RequestMethod.GET)
-	protected void removeTypeMatAttrGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		TypeMaterialAttribute type;
-		type = typeService.find(Integer.parseInt(request.getParameter("id")));
+	protected String removeTypeMatAttrGet(@RequestParam("id") String id) {
+		int typeId = Integer.parseInt(id);
+		TypeMaterialAttribute type = typeService.find(typeId);
+
 		typeService.remove(type);
 
-		RequestDispatcher rd = null;
-		rd = request.getRequestDispatcher("/Config");
-		rd.forward(request, response);
-	}
-
-	@RequestMapping(value = "/RemoveTypeMatAttr", method = RequestMethod.POST)
-	protected void removeTypeMatAttrPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		return "redirect:/Config";
 	}
 }
